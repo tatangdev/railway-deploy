504c6b88bf5a087261d97ac89f4cef321d07294c
tatang <itsme@tatang.dev>
2022-11-17 21:22:23 +0700

    feat: add DB_PORT

--- db/config.js
+++ db/config.js
@@ -4,6 +4,7 @@ const {
   DB_PASSWORD,
   DB_HOST,
   DB_NAME,
+  DB_PORT,
   DB_DIALECT = 'postgres'
 } = process.env;
 
@@ -13,20 +14,23 @@ module.exports = {
     "password": DB_PASSWORD,
     "database": DB_NAME,
     "host": DB_HOST,
-    "dialect": DB_DIALECT
+    "dialect": DB_DIALECT,
+    "port": DB_PORT
   },
   "test": {
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
     "database": DB_NAME,
     "host": DB_HOST,
-    "dialect": DB_DIALECT
+    "dialect": DB_DIALECT,
+    "port": DB_PORT
   },
   "production": {
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
     "database": DB_NAME,
     "host": DB_HOST,
-    "dialect": DB_DIALECT
+    "dialect": DB_DIALECT,
+    "port": DB_PORT
   }
 };
